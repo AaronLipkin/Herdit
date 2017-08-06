5f0596938a9b7ab0ee61b41e4c73b9ad2cdf2dbe
Aaron Lipkin <aj17@me.com>
2017-08-06 15:22:26 -0400

    Authentication for login functioning

---
+++ controllers/usersController.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/postsModel.js');
+const User = require('../models/usersModel.js')
+
+router.get('/new', (req, res) => {
+	res.render('users/new.ejs')
+})
+
+router.post('/new', (req, res) => {
+	User.create(req.body, (err, newUser)=> {
+		console.log(newUser)
+		res.redirect('/users/' + newUser._id)
+	})
+})
+
+
+router.get('/:id', (req, res) => {
+	User.findById(req.params.id, (err, foundUser) => {
+		res.render('users/show.ejs', {
+			user: foundUser
+		})
+	})
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,26 +5,35 @@ const bodyParser     = require('body-parser');
 const methodOverride = require('method-override');
 const User 			 = require('./models/usersModel.js')
 const Post 			 = require('./models/postsModel.js')
-// const session        = require('express-session');
+const session        = require('express-session');
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(session({
+	  secret: "this is a random string secret", //a random string do not copy this value or your stuff will get hacked
+	  resave: false,
+	  saveUninitialized: false
+	 
+}));
 
-// const authorsController = require('./controllers/users.js');
-// app.use('/users', authorsController);
+const usersController = require('./controllers/usersController.js');
+app.use('/users', usersController);
 const postsController = require('./controllers/postsController.js');
 app.use('/posts', postsController);
-// const sessionsController = require('./controllers/session.js');
-// app.use('/sessions', sessionsController);
+const sessionsController = require('./controllers/sessionsController.js');
+app.use('/sessions', sessionsController);
 
 app.get('/', (req, res)=>{
 	Post.find({}, (err, foundPosts) => {
 		res.render('index.ejs', {
-			posts: foundPosts
+			posts: foundPosts,
+			user: req.session.username
 		});
 	})
 });
+
+
 
 mongoose.connect('mongodb://localhost:27017/herdit');
 
